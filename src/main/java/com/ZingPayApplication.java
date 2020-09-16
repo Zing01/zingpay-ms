af2aba3442841160bcef37ac8703186c8fc90848
bilalhassan48 <bilalhassan_bh@outlook.com>
2020-09-16 17:43:31 +0500

    aa

--- src/main/java/com/ZingPayApplication.java
+++ src/main/java/com/ZingPayApplication.java
@@ -11,7 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ZingPayApplication {
     public static void main(String[] args) {
-
         SpringApplication.run(ZingPayApplication.class, args);
     }
 }
